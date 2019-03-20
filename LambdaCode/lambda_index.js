715ba379a80ad042a90a011e914547454a6295db
PriyaSudip <40382124+PriyaSudip@users.noreply.github.com>
2019-03-20 14:35:28 +1100

    update node

--- LambdaCode/lambda_index.js
+++ LambdaCode/lambda_index.js
@@ -7,38 +7,7 @@ exports.handler = (event, context, callback) => {
     var responseBody = event;
     
     var responseCode = 200;
-   // var mainResp = main.CreatePayload();
-   
-   // the below method would return list of cookie in request header
-   /**
-* Receives an array of headers and extract the value from the cookie header
-* @param  {String}   errors List of errors
-* @return {Object}
-*/
-function getCookiesFromHeader(headers) {
 
-    if (headers === null || headers === undefined || headers.Cookie === undefined) {
-        return {};
-    }
-
-    // Split a cookie string in an array (Originally found http://stackoverflow.com/a/3409200/1427439)
-    var list = {},
-        rc = headers.Cookie;
-
-    rc && rc.split(';').forEach(function( cookie ) {
-        var parts = cookie.split('=');
-        var key = parts.shift().trim()
-        var value = decodeURI(parts.join('='));
-        if (key != '') {
-            list[key] = value
-        }
-    });
-
-    return list;
-};
-
-
-var headerCookies = getCookiesFromHeader(event.headers);
 
 
 // creating cookie
@@ -61,104 +30,10 @@ var headerCookies = getCookiesFromHeader(event.headers);
 * ```
 * @return string
 */
-function setCookieString(key, value, options) {
-    var defaults = {
-        secure: true,
-        httpOnly: true,
-        domain: false,
-        path: '/',
-        expires: false,
-        maxAge: false
-    }
-    if (typeof options == 'object') {
-        options = Object.assign({}, defaults, options);
-    } else {
-        options = defaults;
-    }
 
-    var cookie = key + '=' + value;
 
-    if (options.domain) {
-        cookie = cookie + '; domain=' + options.domain;
-    }
 
-    if (options.path) {
-        cookie = cookie + '; path=' + options.path;
-    }
 
-    if (!options.expires && options.maxAge) {
-        options.expires = new Date(new Date().getTime() + parseInt(options.maxAge) * 1000); // JS operate in Milli-seconds
-    }
-
-    if (typeof options.expires == "object" && typeof options.expires.toUTCString) {
-        options.expires = options.expires.toUTCString();
-    }
-
-    if (options.expires) {
-        cookie = cookie + '; expires=' + options.expires.toString();
-   }
-
-    if (options.secure) {
-        cookie = cookie + '; Secure';
-    }
-
-    if (options.httpOnly) {
-        cookie = cookie + '; HttpOnly';
-    }
-
-    return cookie;
-}
-
-var amcv_Cookie;
-
-   
-   //check if cookie exists else create one
-   if(!headerCookies['AMCV_@AdobeOrg']){
-       // logic to create a cookie
-       amcv_Cookie = setCookieString('AMCV_@AdobeOrg','MCMID-2167743609|1234|MCAAMB|abcd1234|MCAAMLH|9');
-       console.log('amcv cookie created in code', amcv_Cookie);
-   } else {
-       amcv_Cookie = headerCookies['AMCV_@AdobeOrg'];
-       console.log('cookie was available in Request Headers', headerCookies['AMCV_@AdobeOrg']);
-   }
-   
-    
-    
-   
-     var mainResp = main.CreatePayload(amcv_Cookie,"mock-test-int");
-     console.error("test1:"+mainResp);
-     
-     
-     
-     
-    //setting cookie
-    
-     console.log("{'Cookie':event['Cookie']}");
-    var date = new Date();
-
-  // Get Unix milliseconds at current time plus 365 days
-  date.setTime( 1332403882588); 
-  var cookieVal = Math.random().toString(36).substring(7); // Generate a random cookie string
-  
-  var cookieString = "myCookie="+cookieVal+"; domain=my.domain; expires="+date.toGMTString()+";";
-  console.log("{'Cookie':event['Cookie']}"+cookieString);
-  
-  
-  
-    
-    var response = {
-        statusCode: responseCode,
-        headers: {
-            "x-custom-header" : "cookie test",
-            "cookie" : "custom cookie"
-            
-        },
-        
-        body: JSON.stringify(mainResp)
-    };
-
-    console.log("Got this cookie:", event.cookie);
-    console.log("response: " + JSON.stringify(response));
     callback(null, response);
 };
 
